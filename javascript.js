8ea8fb005c995c0a29210b9b9b4a8597bf4d38bd
João Alves <jvamarangonia@gmail.com>
2019-10-15 21:16:03 -0300

    finalizado

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+var exec_oper = 1;
+var exec_ponto = 1;
+
 function resetar_tela(){
     document.getElementById("resultado").value = null;
 }
@@ -9,13 +12,31 @@ function pega_numero(num){
 
 function pega_operacao(operador){
     var tela = document.getElementById("resultado");
-    if (!(operador in tela.value.split(''))){
-        tela.value = tela.value + operador
+    if (exec_oper == 1){
+        tela.value = tela.value + operador;
+        exec_oper += 1;
+    }
+}
+
+function pega_ponto(operador){
+    var tela = document.getElementById("resultado");
+    if (exec_ponto == 1){
+        tela.value = tela.value + operador;
+        exec_ponto += 1;
+    } else if (exec_oper == 2 && exec_ponto == 2){
+        tela.value = tela.value + operador;
+        exec_ponto += 1;
     }
 }
 
 function calcular(){
     var tela = document.getElementById("resultado");
-    var resultado = eval(tela.value)
-    tela.value = resultado
+    var ultimo_item = tela.value.length - 1;
+
+    if (isNaN(tela.value[ultimo_item])){
+        tela.value = "_Erro de operação_"
+    } else {
+        var resultado = eval(tela.value);
+        tela.value = resultado
+    }
 }
